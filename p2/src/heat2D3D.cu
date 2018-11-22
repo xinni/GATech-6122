32295afb9fd9550945d2fb48a0ee502e4edc5bf2
xinni <xinni@buffalo.edu>
2018-11-22 01:01:51 -0500

    set init temp

--- p2/src/heat2D3D.cu
+++ p2/src/heat2D3D.cu
@@ -4,19 +4,35 @@
 #include <stdlib.h>
 
 using namespace std;
-//#define k 0.5f      // only can use define?
-#define width 10
+#define width 5
 #define height 10
 
 __device__ float k = 0.5;
 int timeSteps = 10;
-// int width = 10;
-// int height = 10;
 int N = width * height;
 float startingTemp = 0;
-int location_x, location_y, location_z = 0;
-float fixedTemp = 5;
-int DIM = 16;
+// int DIM = 10;
+
+// #location_x, location_y, width, height, fixed temperature 5, 5, 20, 20, 200
+// 500, 500, 10, 10, 300
+void setInitHeatMap(float *dst, char str[]) {
+    // set 5, 5, 2, 2, 10
+    int location_x = 2;
+    int location_y = 4;
+    int widthFix= 4;
+    int heightFix = 2;
+    float fixedTemp = 10;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if ((i >= location_y) && (i < location_y + heightFix)) {
+                if ((j >= location_x) && (j < location_x + widthFix)){
+                    dst[i * width + j] = fixedTemp;
+                }
+            }
+        }
+    }
+}
 
 __global__ void update_2D_kernel (float *dst, float *src) {
     int x = threadIdx.x + blockIdx.x * blockDim.x;
@@ -33,34 +49,32 @@ __global__ void update_2D_kernel (float *dst, float *src) {
     if (y == 0) top += width;
     if (y == width - 1) bottom -= width;
   
-    dst[index] = src[index] + k * (src[top] +  
-                     src[bottom] + src[left] + src[right] -  
-                     src[index]*4);
+    dst[index] = src[index] + k * 
+            (src[top] + src[bottom] + src[left] + src[right] - src[index] * 4);
 }
 
 int main (void) {
-    float current[width][height] = {0};
-    float previous[width][height] = {0};
+    float fix[width * height] = {0};
+    float current[width * height] = {0};
+    float previous[width * height] = {0};
 
     float *d_c, *d_p;
+
+    setInitHeatMap(fix, "500, 500, 10, 10, 300");
 
     cudaMalloc((void**)&d_c, N * sizeof(float));
     cudaMalloc((void**)&d_p, N * sizeof(float));
 
-    dim3 blocks(DIM/16,DIM/16);
-    dim3 threads(16,16);
+    dim3 blocks(1,1);
+    dim3 threads(5,10);
     
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            cout << current[i][j];
-            if (j == width - 1) {
-                cout << endl;
-            } else {
-                cout << ", ";
-            }
+    for (int i = 0; i < height * width; i++) {
+        if (i % width != width - 1) {
+            cout << fix[i] << ", ";
+        } else {
+            cout << fix[i] << endl;
         }
     }
-    // cout << "k = " << k << endl;
 
     return 1;
 }
