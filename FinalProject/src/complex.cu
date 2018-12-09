9e1cc40aba42e60466f4016764c080fa14887dd6
xinni <xinni@buffalo.edu>
2018-12-09 02:57:23 -0500

    use prof method

--- FinalProject/src/complex.cu
+++ FinalProject/src/complex.cu
@@ -3,51 +3,52 @@
 //
 
 #include "complex.h"
+#include <cuda.h>
 
 #include <cmath>
 
 const float PI = 3.14159265358979f;
 
-Complex::Complex() : real(0.0f), imag(0.0f) {}
+__host__ __device__ Complex::Complex() : real(0.0f), imag(0.0f) {}
 
-Complex::Complex(float r) : real(r), imag(0.0f) {}
+__host__ __device__ Complex::Complex(float r) : real(r), imag(0.0f) {}
 
-Complex::Complex(float r, float i) : real(r), imag(i) {}
+__host__ __device__ Complex::Complex(float r, float i) : real(r), imag(i) {}
 
-Complex Complex::operator+(const Complex &b) const {
+__host__ __device__ Complex Complex::operator+(const Complex &b) const {
     float newReal = real + b.real;
     float newImag = imag + b.imag;
     Complex newComplex(newReal, newImag);
     return newComplex;
 }
 
-Complex Complex::operator-(const Complex &b) const {
+__host__ __device__ Complex Complex::operator-(const Complex &b) const {
     float newReal = real - b.real;
     float newImag = imag - b.imag;
     Complex newComplex(newReal, newImag);
     return newComplex;
 }
 
-Complex Complex::operator*(const Complex &b) const {
+__host__ __device__ Complex Complex::operator*(const Complex &b) const {
     float newReal = real * b.real - imag * b.imag;
     float newImag = real * b.imag + imag * b.real;
     Complex newComplex(newReal, newImag);
     return newComplex;
 }
 
-Complex Complex::mag() const {
+__host__ __device__ Complex Complex::mag() const {
     float magNum = sqrt(real * real + imag * imag);
     Complex magComplex(magNum);
     return magComplex;
 }
 
-Complex Complex::angle() const {
+__host__ __device__ Complex Complex::angle() const {
     float angle = atan(1.0 * imag / real)*180/PI;
     Complex angleComplex(angle);
     return angleComplex;
 }
 
-Complex Complex::conj() const {
+__host__ __device__ Complex Complex::conj() const {
     Complex newComplex(real, -1.0 * imag);
     return newComplex;
 }
--- FinalProject/src/fft2d.cu
+++ FinalProject/src/fft2d.cu
@@ -3,28 +3,45 @@
 #include <string>
 #include <stdlib.h>
 #include <math.h>
+#include <cuda.h>
 #include "complex.h"
 #include "input_image.h"
 
 #define DIM 32
 
+const float PI = 3.14159265358979f;
+
 using namespace std;
 
-/*************** transform by row **********************/
-// __global__ void transByRow (complex *dst, complex *src, int width, int height) {
-//     int x = threadIdx.x + blockIdx.x * blockDim.x;
-//     int y = threadIdx.y + blockIdx.y * blockDim.y;
-//     int index = x + y * width;
+struct DeviceData {
+    Complex *d_data;
+    Complex *d_res;
+};
 
-//     if (x < width && y < height) {
-//         for (int i = 0; i < width; i++) {
-//             dst[index]->real += (src[i]->real * cos((2*PI*index*i) / width))/width;
-//             dst[index]->imag += -(src[i]->real * sin((2*PI*index*i) / width))/width;
-//         }
-//     }
-// }
+void cleanup(DeviceData *d) {
+    cudaFree(d->d_data);
+    cudaFree(d->d_res);
+}
+
+/*************** transform by row **********************/
+__global__ void transByRow (Complex* dst, Complex* src, int width, int height) {
+    int x = threadIdx.x + blockIdx.x * blockDim.x;
+    int y = threadIdx.y + blockIdx.y * blockDim.y;
+    int index = x + y * width;
+
+    if (x < width && y < height) {
+        dst[index] = 3;
+        // for (int i = 0; i < width; i++) {
+        //     // (dst + index)->real += ((src + i)->real * cos((2*PI*index*i) / width))/width;
+        //     // (dst + index)->imag += -((src + i)->real * sin((2*PI*index*i) / width))/width;
+        //     dst[index] = 3;
+        // }
+    }
+}
 
 int main (int argc, char* argv[]) {
+    DeviceData devs;
+
     char* inFile="Tower256.txt";
     char* outFile="Mytest.txt";
 
@@ -38,10 +55,29 @@ int main (int argc, char* argv[]) {
     int width = image.get_width();
     int N = height * width;
 
-    Complex src[N];
+    // Complex src[N];
     Complex res[N];
-    
+    fill_n(res, N, 1);
+
     Complex* data = image.get_image_data();
+
+    cudaMalloc((void**)&devs.d_data, N * sizeof(Complex));
+    cudaMalloc((void**)&devs.d_res, N * sizeof(Complex));
+
+    cudaMemcpy(devs.d_data, data, N * sizeof(Complex), cudaMemcpyHostToDevice);
+
+    dim3 blocks((width + DIM - 1) / DIM, (height + DIM - 1) / DIM);
+    dim3 threads(DIM, DIM);
+
+    cout << width << ", " << height << endl;
+
+    transByRow<<<blocks, threads>>>(devs.d_res, devs.d_data, width, height);
+
+    cudaMemcpy(res, devs.d_res, N*sizeof(Complex), cudaMemcpyDeviceToHost);
+
+    image.save_image_data("MyAfter2D.txt", res, width, height);
+
+    cleanup(&devs);
 
     return 0;
 }
